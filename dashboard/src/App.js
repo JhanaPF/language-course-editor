0b0f990eb5d6b21bb72a52ad92c19add0391f624
Jhana <mrgamagejhana@gmail.com>
2025-07-22 23:19:21 +0200

    Update App.js

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,86 +1,84 @@
-import './css/App.css'
-import React from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './css/App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
-import Login from './views/Login'
-import { post } from './api/apiRequests'
+import Login from './views/Login';
+import { post } from './api/apiRequests';
 import CoursesOverview from './views/CoursesOverview';
 import LessonsOverview from './views/LessonsOverview';
 import QuestionsOverview from './views/QuestionsOverview';
 import Layout from './components/wrappers/Layout';
 
-class App extends React.Component {
-    constructor() {
-        super()
+function App() {
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [loading, setLoading] = useState(true);
 
-        this.state = {
-            uId: '',
-            loggedin: false,
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        // Décommente et adapte cette logique selon besoin
+        /*
+        const isLoggedIn = sessionStorage.getItem('isLoggedIn');
+        if (isLoggedIn) {
+          setLoggedIn(true);
+          setLoading(false);
+          return;
         }
-    }
+    
+        const isIdSaved = localStorage.getItem('keepConnection');
+        if (isIdSaved) {
+          const mail = localStorage.getItem('mail');
+          const password = localStorage.getItem('password');
+          signIn(mail, password);
+          return;
+        }
+    
+        post('auth/token',
+          () => unlockApp(),
+          () => setLoading(false)
+        );
+        */
+    }, []);
 
-    componentDidMount() {
-
-        //const isIdSaved = localStorage.getItem('keepConnection')
-        //const isLoggedIn = sessionStorage.getItem('isLoggedIn')
-
-        //if (isLoggedIn) {
-        //    return this.setState({ loggedin: true, loading: false })
-        //}
-
-        //if (isIdSaved) {
-        //    const mail = localStorage.getItem('mail')
-        //    const password = localStorage.getItem('password')
-        //    this.signIn(mail, password)
-        //    return
-        //}
-
-        //post('auth/token',
-        //    () => this.unlockApp(), // Token still valid
-        //    () => { // No token or not valid anymore
-        //        console.log("No token")
-        //        this.setState({ loading: false })
-        //    }
-        //)
-    }
-
-    signIn = (mail, password) => {
-
+    const signIn = (mail, password) => {
         post('auth/signin', { mail, password },
             (res) => {
-                console.log(res)
-                localStorage.setItem('userId', res.data.userId)
-                this.unlockApp()
+                localStorage.setItem('userId', res.data.userId);
+                unlockApp();
             },
-            () => this.setState({ loading: false })
-        )
-    }
+            () => setLoading(false)
+        );
+    };
 
-    unlockApp() {
-        console.log("logged in")
-        this.setState({ loggedin: true })
-        window.location.href = "/courses";
-        sessionStorage.setItem('isLoggedIn', true)
-    }
+    const unlockApp = () => {
+        setLoggedIn(true);
+        sessionStorage.setItem('isLoggedIn', true);
+        navigate('/courses');
+    };
 
-    render() {
-        return (
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Login signIn={this.signIn.bind(this)} />} />
+    return (
+        <Routes>
+            <Route path="/" element={<Login signIn={signIn} />} />
 
-                    <Route element={<Layout />}>
-                        <Route path="/courses" element={<CoursesOverview/>} />
-                        <Route path="/lessons/:courseId" element={<LessonsOverview/>} />
-                        <Route path="/questions/:lessonId" element={<QuestionsOverview/>} />
-                    </Route>
-                </Routes>
-            </BrowserRouter>
-        )
-    }
+            <Route element={<Layout />}>
+                <Route path="/courses" element={<CoursesOverview />} />
+                <Route path="/lessons/:courseId" element={<LessonsOverview />} />
+                <Route path="/questions/:lessonId" element={<QuestionsOverview />} />
+            </Route>
+        </Routes>
+    );
 }
 
-export default App
+// Composant wrapper pour intégrer le Router (car `useNavigate` ne fonctionne qu'à l'intérieur d'un `<BrowserRouter>`)
+function AppWithRouter() {
+    return (
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    );
+}
+
+export default AppWithRouter;
