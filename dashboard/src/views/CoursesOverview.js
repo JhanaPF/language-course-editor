7a41b863ef4873f32f84f6d57e04bee2d370a090
Jhana <mrgamagejhana@gmail.com>
2025-07-24 11:19:06 +0200

    Update CoursesOverview.js

--- dashboard/src/views/CoursesOverview.js
+++ dashboard/src/views/CoursesOverview.js
@@ -1,99 +1,78 @@
-import React from 'react'
-import { Row, Col } from 'reactstrap'
-import LessonsOverview from './LessonsOverview'
-import AddButton from '../components/buttons/AddButton'
-import ReturnButton from '../components/buttons/ReturnButton'
-import QuestionsOverview from './QuestionsOverview'
-import CourseModal from '../modals/CourseModal'
-import { get } from '../api/apiRequests'
-import ClickableCard from '../components/cards/ClickableCard'
+import React, { useEffect, useState } from 'react';
+import { Row, Col } from 'reactstrap';
 
-class CoursesOverview extends React.Component { // Show all courses
-    
-    constructor(props) {
-        super(props)
+import LessonsOverview from './LessonsOverview';
+import QuestionsOverview from './QuestionsOverview';
+import AddButton from '../components/buttons/AddButton';
+import ReturnButton from '../components/buttons/ReturnButton';
+import CourseModal from '../modals/CourseModal';
+import ClickableCard from '../components/cards/ClickableCard';
 
-        this.state = {
-            course: undefined,
-            loading: true
-        }
+import { get } from '../api/apiRequests';
 
-        this.toggleCourseModal = this.toggleCourseModal.bind(this)
-        this.handleChange = this.handleChange.bind(this)
+function CoursesOverview() {
+    const [courses, setCourses] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [courseId, setCourseId] = useState(undefined);
+    const [courseModal, setCourseModal] = useState(false);
+
+    useEffect(() => {
+        fetchCourses();
+    }, []);
+
+    const initState = (res) => {
+        setCourses(res.data.courses);
+        setLoading(false);
     }
 
-    componentDidMount() {
-        this.fetchCourses()
-    }
+    const fetchCourses = () => {
+        get('courses', {}, (res) => initState(res), () => this.setState({ loading: false, courseModal: false }))
+    };
 
-    fetchCourses() {
-        get('courses', {}, (res) => this.initState(res), () => this.setState({ loading: false, courseModal: false }))
-    }
+    const toggleCourseModal = () => {
+        setCourseModal(!courseModal);
+    };
 
-    initState(res) {
-        this.setState({ courses: res.data.courses, loading: false, courseModal: false })
-    }
+    const getCourse = (id) => {
+        return courses.find((course) => course._id === id);
+    };
 
-    handleSelectChange = (param, e) => {
-        this.setState({ [param]: e })
-    }
+    const displayedCourses = courseId
+        ? courses.filter((course) => course._id === courseId)
+        : courses;
 
-    handleChange(event) {
-        const { name, value } = event.currentTarget
-        this.setState({ [name]: value })
-    }
+    if (loading) return null;
 
-    toggleCourseModal() {
-        this.setState({ courseModal: !this.state.courseModal })
-    }
-
-    setCourse(id) {
-        this.setState({ courseId: id })
-    }
-
-    getCourse(id) {
-        return this.state.courses.find(course => course._id === id)
-    }
-
-    render() {
-        if (!this.state.courses) return null
-
-        const courses = this.state.courseId ? this.state.courses.filter(course => course._id === this.state.courseId) : this.state.courses
-
-        return (<>
-            {!this.state.courseId &&
-                <Row className='w-100 mt-5'>
-                    <Col className='text-right ml-auto'>
-                        <AddButton addFunction={this.toggleCourseModal.bind(this)}>Ajouter un cours</AddButton>
+    return (
+        <>
+            {!courseId && (
+                <Row className="w-100 mt-5">
+                    <Col className="text-right ml-auto">
+                        <AddButton addFunction={toggleCourseModal}>Ajouter un cours</AddButton>
                     </Col>
                 </Row>
-            }
+            )}
 
-            {this.state.courseId !== undefined &&
+            {courseId !== undefined && (
                 <Row>
-                    <ReturnButton goBack={function () { this.setCourse(undefined) }.bind(this)} />
+                    <ReturnButton goBack={() => setCourseId(undefined)} />
                 </Row>
-            }
+            )}
 
-            <div className='mt-3 d-flex w-100 justify-content-center'>
-                {courses.map((course, index) =>
-                    <ClickableCard key={index} type="course" item={course} onClick={this.setCourse.bind(this)}/>
-                )}
+            <div className="mt-3 d-flex w-100 justify-content-center">
+                {displayedCourses.map((course, index) => (
+                    <ClickableCard key={index} type="course" item={course} onClick={setCourseId} />
+                ))}
             </div>
 
-            {this.state.courseModal &&
-                <CourseModal closeModal={this.toggleCourseModal} fetchCourses={this.fetchCourses.bind(this)} />
-            }
+            {courseModal && (
+                <CourseModal closeModal={toggleCourseModal} fetchCourses={fetchCourses} />
+            )}
 
-            {this.state.courseId &&
-                <LessonsOverview course={this.getCourse(this.state.courseId)} />
-            }
+            {courseId && <LessonsOverview course={getCourse(courseId)} />}
 
-            {this.state.lesson &&
-                <QuestionsOverview />
-            }
-        </>)
-    }
+        </>
+    );
 }
 
-export default CoursesOverview
+export default CoursesOverview;
