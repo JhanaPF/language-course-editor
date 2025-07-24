f52586552cc9b8c8c138b45eee74306a7bb1d588
Jhana <mrgamagejhana@gmail.com>
2025-07-24 11:59:16 +0200

    Update Overview.js

--- dashboard/src/common/Overview.js
+++ dashboard/src/common/Overview.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useCallback, useEffect, useState } from 'react';
 import { get } from '../api/apiRequests'
 
 /**
@@ -6,55 +6,56 @@ import { get } from '../api/apiRequests'
  * Call componentDidMount with super.onFetch(filter)
  * In render add OverviewWrapper with nested modal
  */
-export default class Overview extends React.Component {
-    constructor (props, filter, objName) {
-        super(props)
-        this.state = {
-            elements: null,
-            elemId: null,
-            modal: false
+
+export default function Overview(objName, filter) {
+    const [elements, setElements] = useState(null)
+    const [elemId, setElemId] = useState(null)
+    const [element, setElementState] = useState(null)
+    const [modal, setModal] = useState(false)
+
+    const onFetch = useCallback(() => {
+        const initState = (fields = {}) => {
+            setModal(false)
+            setElements(fields.elements || null)
         }
-        this.filter = filter
-        this.objName = objName
-        this.closeModal = this.closeModal.bind(this)
-        this.openModal = this.openModal.bind(this)
-        this.toggleModal = this.toggleModal.bind(this)
-        this.closeModalAfterRequest = this.closeModalAfterRequest.bind(this)
-        this.setElement = this.setElement.bind(this)
-        this.getElement = this.getElement.bind(this)
+
+        get(objName, filter,
+            (res) => initState({ elements: res.data[objName] }),
+            () => initState()
+        )
+    }, [objName, filter])
+
+    useEffect(() => {
+        onFetch()
+    }, [onFetch])
+
+    const toggleModal = () => setModal(prev => !prev)
+    const closeModal = () => setModal(false)
+    const openModal = () => setModal(true)
+
+    const closeModalAfterRequest = () => {
+        onFetch()
     }
 
-    componentDidMount () {
-        this.onFetch()
+    const setElement = (id) => {
+        const el = elements?.find(e => e._id === id)
+        setElemId(id)
+        setElementState(el || null)
     }
 
-    onFetch () {
-        const initState = (fields) => { this.setState({ loading: false, modal: false, ...fields }) }
-        get(this.objName, this.filter, (res) => initState({ elements: res.data[this.objName] }), () => initState())
-    }
+    const getElement = (id) => elements?.find(e => e._id === id)
 
-    toggleModal = () => this.setState({ modal: !this.state.modal })
-
-    closeModalAfterRequest () {
-        this.onFetch()
-    }
-
-    closeModal () {
-        this.setState({ modal: false })
-    }
-
-    openModal () {
-        this.setState({ modal: true })
-    }
-
-    setElement (id) {
-        this.setState({
-            elemId: id,
-            element: this.getElement(id)
-        })
-    }
-
-    getElement (id) {
-        return this.state.elements.find(e => e._id === id)
+    return {
+        elements,
+        elemId,
+        element,
+        modal,
+        toggleModal,
+        closeModal,
+        openModal,
+        closeModalAfterRequest,
+        setElement,
+        getElement
     }
 }
+
